425fc265b99624cbdb9987f11de05e8fe596eec3
Tony Yang <superzoro168@gmail.com>
2018-12-28 18:50:13 +0800

    keep screen on

--- app/src/main/java/com/tonyyang/gaming/csgomap/MainActivity.kt
+++ app/src/main/java/com/tonyyang/gaming/csgomap/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tonyyang.gaming.csgomap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         viewpager.apply {
             adapter = MapAdapter(supportFragmentManager, mDataList)
             overScrollMode = View.OVER_SCROLL_ALWAYS
