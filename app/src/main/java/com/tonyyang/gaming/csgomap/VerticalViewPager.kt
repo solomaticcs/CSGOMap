7aa56a696db7d83d7c2a06921bed254ffafdcf82
Tony Yang <superzoro168@gmail.com>
2019-12-02 13:27:54 +0800

    Fix scrolling bug

--- app/src/main/java/com/tonyyang/gaming/csgomap/VerticalViewPager.kt
+++ app/src/main/java/com/tonyyang/gaming/csgomap/VerticalViewPager.kt
@@ -9,11 +9,10 @@ import java.io.Serializable
 import android.view.MotionEvent
 
 
-
 /**
- * @author tonyyang
+ * Reference to this link:
+ * https://android.googlesource.com/platform/packages/apps/DeskClock/+/master/src/com/android/deskclock/VerticalViewPager.java
  */
-
 class VerticalViewPager @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null
 ) : ViewPager(context, attrs), Serializable {
@@ -23,37 +22,52 @@ class VerticalViewPager @JvmOverloads constructor(
         overScrollMode = View.OVER_SCROLL_NEVER
     }
 
-    private inner class VerticalPageTransformer : ViewPager.PageTransformer {
-
-        override fun transformPage(view: View, position: Float) {
-            when {
-                position < -1 -> view.alpha = 0f
-                position <= 1 -> {
-                    view.alpha = 1f
-                    view.translationX = view.width * -position
-                    val yPosition = position * view.height
-                    view.translationY = yPosition
-                }
-                else -> view.alpha = 0f
-            }
-        }
-    }
-
     private fun swapXY(ev: MotionEvent): MotionEvent {
-        val newX = ev.y / height * width
-        val newY = ev.x / width * height
+        val newX = (ev.y / height) * width
+        val newY = (ev.x / width) * height
         ev.setLocation(newX, newY)
         return ev
     }
 
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
-        return super.onInterceptTouchEvent(swapXY(ev)).apply {
-            swapXY(ev)
-        }
+        val intercepted = super.onInterceptTouchEvent(swapXY(ev))
+        swapXY(ev)
+        return intercepted
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(ev: MotionEvent): Boolean {
-        return super.onTouchEvent(swapXY(ev))
+        val handle = super.onTouchEvent(swapXY(ev))
+        swapXY(ev)
+        return handle
+    }
+
+    override fun canScrollHorizontally(direction: Int): Boolean {
+        return false
+    }
+
+    override fun canScrollVertically(direction: Int): Boolean {
+        return super.canScrollHorizontally(direction)
+    }
+
+    private inner class VerticalPageTransformer : PageTransformer {
+
+        override fun transformPage(view: View, position: Float) {
+            val pageWidth = view.width
+            val pageHeight = view.height
+            when {
+                position < -1 -> {
+                    view.alpha = 0F
+                }
+                position <= 1 -> {
+                    view.alpha = 1F
+                    view.translationX = pageWidth * -position
+                    view.translationY = pageHeight * position
+                }
+                else -> {
+                    view.alpha = 0F
+                }
+            }
+        }
     }
 }
